1076f8f4c6404960316db7e23f8fae8232b8b083
tongl <litong@gdgyun.com>
2018-08-02 13:32:55 +0800

    test plugins

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import React from 'react'
+import App from './App'
+import ReactDom from 'react-dom'
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'add':
+      return state + 1
+    case 'minus':
+      return state - 1
+    default:
+      return state
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(counterReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension : f => f))
+
+ReactDom.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>),
+  document.getElementById('root')
+)
